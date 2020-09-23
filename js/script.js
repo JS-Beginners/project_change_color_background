4ad2ec57cbc4f53b33188bc77149dc3b8310aab1
shashwath KR <email@shashwath.com>
2020-09-23 22:42:24 +0530

    text deleted

--- js/script.js
+++ js/script.js
@@ -6,6 +6,5 @@ const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
 
 button[0].addEventListener('click', function () {
 const randomcolor= colors[Math.floor(Math.random()*colors.length)];
-body[0].style.background= randomcolor;
-return Math.PI; 
+body[0].style.background= randomcolor; 
 });
