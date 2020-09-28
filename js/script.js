7b5ebecac411da86b85ce20214a18ff3deb5db4e
Keshav Bajaj <66621502+keshav-bajaj@users.noreply.github.com>
2020-09-28 22:07:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Choose a random color
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple' , 'lime' , 'maroon' , 'navy' , 'aqua' , 'olive' , 'silver' , 'teal' , 'rebeccapurple']
 
 body.style.backgroundColor = 'violet'
 button.addEventListener('click', changeBackground)
