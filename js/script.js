018d826fc5002858c6f85865485d066f9ae3029a
keshav-bajaj <bajajkeshav111@gmail.com>
2020-09-28 22:25:28 +0530

    add more colors

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Choose a random color
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple' , 'lime' , 'maroon' , 'navy' , 'aqua' , 'olive' , 'silver' , 'teal' , 'rebeccapurple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple' , 'lime' , 'maroon' , 'navy' , 'aqua' , 'olive' , 'silver' , 'teal' , 'rebeccapurple' , 'fuchsia' , 'coral' , 'turquoise' , 'magenta' , 'salmon' , 'orchid' , 'dodgerblue' , 'plum' , 'tomato' , 'khaki' , 'gold' , 'orange' , 'seagreen' , 'burlywood' , 'brown' , 'cadetblue' , 'crimson' , 'cyan' , 'cornsilk' , 'firebrick' , 'deepskyblue' , 'seashell' , 'saddlebrown' , 'forestgreen' , 'floralwhite' , 'ivory' , 'indigo' , 'lavender' , 'olivedrab' , 'orangered' , 'sandybrown' , 'steelblue' , 'wheat' , 'slateblue' , 'springgreen' , 'tan']
 
 body.style.backgroundColor = 'violet'
 button.addEventListener('click', changeBackground)
