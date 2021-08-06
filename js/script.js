c1fc6ba5a926ffe3ebba6ee2b1b715bbb38aa383
Mohammad Atallah <mohamedatallahh2017@gmail.com>
2021-08-06 07:09:26 +0300

    Generate random background with a dynamic wide range

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector("button");
+const body = document.querySelector("body");
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+changeBackground();
+button.addEventListener("click", changeBackground);
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+function changeBackground() {
+  const h = parseInt(Math.random() * 360);
+  body.style.backgroundColor = `hsl(${h}, 60%, 45%)`;
 }
