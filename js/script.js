08f2b9e5ab26cd46d4f53475b3e853bdd36e9305
Ayush Tiwari <ayushtiwari820@gmail.com>
2021-09-12 18:09:49 +0530

    improved range of colors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //Choose a random color
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
 
 body.style.backgroundColor = 'violet'
 button.addEventListener('click', changeBackground)
 
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+const red = parseInt(Math.random()*256);
+const green = parseInt(Math.random()*256);
+const blue = parseInt(Math.random()*256);
+
+body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
