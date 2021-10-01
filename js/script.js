48b32feeba3310d74096a048f2fe8368f9c6b713
Ayush Tiwari <ayushtiwari820@gmail.com>
2021-10-01 08:50:27 +0530

    fixed BGcolor of button to contrast out of background

--- js/script.js
+++ js/script.js
@@ -12,5 +12,14 @@ const red = parseInt(Math.random()*256);
 const green = parseInt(Math.random()*256);
 const blue = parseInt(Math.random()*256);
 
+let Bred = 255,Bgreen = 255,Bblue = 255,Btextcolor = 0;
+if(red === 255 && green === 255 && blue === 255){
+    [Bred,Bgreen,Bblue] = 0;
+    Btextcolor = 255;
+}
+
+button.style.backgroundColor = `rgb(${Bred}, ${Bgreen}, ${Bblue})`;
+button.style.Color = `rgb(${Btextcolor}, ${Btextcolor}, ${Btextcolor})`;
+
 body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
