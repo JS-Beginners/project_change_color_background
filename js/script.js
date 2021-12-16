27ca37744710f35020c276341acfc2dc4391b224
MukulsinghRajpurohit <88364925+MukulsinghRajpurohit@users.noreply.github.com>
2021-12-16 22:32:13 +0530

    Replace hard color to there  CSS HEX  values
    change color to there HEX  values and all semicolons at end of every statement .

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['#ff0000', '#3cb371', '#0000ff', '#ffa500', '#ff6375', '#6a5acd'];
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+body.style.backgroundColor = 'violet';
+button.addEventListener('click', changeBackground);
 
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+const colorIndex= parseInt(Math.random()*colors.length);
+body.style.backgroundColor = colors[colorIndex];
 }
