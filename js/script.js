7f109a9214b5caaf6bbd4e3288ca576da533a8d6
Brian Ndickers <47857858+bryondickers@users.noreply.github.com>
2022-05-27 01:35:25 +0300

    arrow function implemented and math,floor used

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ const body = document.querySelector('body')
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
 body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
-
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
+button.addEventListener('click', () =>  {
+const colorIndex = Math.floor((Math.random()*colors.length))
 body.style.backgroundColor = colors[colorIndex]
 }
+)
